746ee404b74189ee365d3fb97f09d040682e3cc4
rbgreenway3rd <3rdgreenway@gmail.com>
2021-05-04 16:00:58 -0500

    cleaned up pre-existing code. Spent a lot of time re-reading old code to figure out next task. Need assistance in taking next step.

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -2,15 +2,12 @@ import { getCustomOrders } from "./database.js"
 
 const buildOrderListItem = (order) => {
     return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
+        Order #${order.id} was placed on ${order.timestamp} 
     </li>`
 }
 
 export const Orders = () => {
-    /*
-        Can you explain why the state variable has to be inside
-        the component function for Orders, but not the others?
-    */
+    
     const orders = getCustomOrders()
 
     let html = "<ul>"
